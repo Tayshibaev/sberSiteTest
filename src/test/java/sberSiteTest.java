a057008e42299f2374c7d987d5cccfc10f37156b
Timur <tayshibaevtb@gmail.com>
2020-03-27 00:07:06 +0300

    Готовый тест версия 1

--- src/test/java/sberSiteTest.java
+++ src/test/java/sberSiteTest.java
@@ -1,4 +1,5 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -8,6 +9,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.concurrent.TimeUnit;
 
 public class sberSiteTest {
@@ -19,7 +24,7 @@ public class sberSiteTest {
         System.setProperty("webdriver.gecko.driver", "drv/geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);//задаем тайм аут по умолчанию ожидания поиска
+        driver.manage().timeouts().implicitlyWait(15L, TimeUnit.SECONDS);//задаем тайм аут по умолчанию ожидания поиска
         driver.manage().window().maximize();
     }
 
@@ -27,16 +32,10 @@ public class sberSiteTest {
     public void test() {
         driver.get(url);
 
-        //driver.findElement(By.xpath("//a[text()='Страхование']")).click();
         driver.findElement(By.xpath("//span[text()='Страхование']")).click();
         driver.findElement(By.xpath("//ul[@class='lg-menu__sub-list']//a[text()='Страхование путешественников']")).click();
-        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 15000);
+        Wait<WebDriver> wait = new WebDriverWait(driver, 3, 15000);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(@class,'top_40')]//h2[text()='Страхование путешественников']"))));
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
 
         driver.findElement(By.xpath("//b[text()='Оформить онлайн']")).click();
 
@@ -51,31 +50,52 @@ public class sberSiteTest {
         }
 
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//button[text()='Оформить']")))).click();
-
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//legend[contains(text(),'Застрахованные')]"))));
-        driver.findElement(By.id("surname_vzr_ins_0")).sendKeys("Ivan");
-        fillField(By.id("surname_vzr_ins_0"),"Ivanushka");
-       // fillField(By.id("birthDate_vzr_ins_0"),"08.07.1994");
-        fillField(By.id("name_vzr_ins_0"),"English");
 
-        driver.findElement(By.id("birthDate_vzr_ins_0")).sendKeys("08.07.1994");
-        driver.findElement(By.id("surname_vzr_ins_0")).click();
-        driver.findElement(By.id("person_birthDate")).sendKeys("08.07.1994");
-        driver.findElement(By.id("surname_vzr_ins_0")).click();
-        //fillField(By.id("person_birthDate"),"08.07.1994");
+        //Даты рождения
+        Calendar dateBirthPeople = new GregorianCalendar(1999,02,13);
+        Calendar dateBirthPerson = new GregorianCalendar(1995,04,15);
+
+        SimpleDateFormat smp = new SimpleDateFormat("dd.MM.YYYY");
+        //Заполнение полей
+        fillField(By.id("surname_vzr_ins_0"),"Ivanushka");
+        fillField(By.id("name_vzr_ins_0"),"English");
+        fillFieldDate(By.id("birthDate_vzr_ins_0"),dateBirthPeople.getTime());
+        fillFieldDate(By.id("person_birthDate"),dateBirthPerson.getTime());
         fillField(By.id("person_lastName"),"Василий");
         fillField(By.id("person_firstName"),"Петрович");
-
         fillField(By.id("person_middleName"),"Магаданович");
-
-
         driver.findElement(By.xpath("//label[text()='Женский']")).click();
 
+
+        Assert.assertEquals("Ivanushka",
+                driver.findElement(By.id("surname_vzr_ins_0")).getAttribute("value"));
+        Assert.assertEquals("English",
+                driver.findElement(By.id("name_vzr_ins_0")).getAttribute("value"));
+
+        Assert.assertEquals("Василий",
+                driver.findElement(By.id("person_lastName")).getAttribute("value"));
+        Assert.assertEquals("Петрович",
+                driver.findElement(By.id("person_firstName")).getAttribute("value"));
+        Assert.assertEquals("Магаданович",
+                driver.findElement(By.id("person_middleName")).getAttribute("value"));
+
+        Assert.assertEquals(smp.format(dateBirthPeople.getTime()),
+                driver.findElement(By.id("birthDate_vzr_ins_0")).getAttribute("value"));
+        Assert.assertEquals(smp.format(dateBirthPerson.getTime()),
+                driver.findElement(By.id("person_birthDate")).getAttribute("value"));
+
+        driver.findElement(By.xpath("//button[contains(text(),'Продолжить')]")).click();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@class='alert-form alert-form-error']"))));
+
+        Assert.assertEquals("При заполнении данных произошла ошибка",
+                driver.findElement(By.xpath("//div[@class='alert-form alert-form-error']")).getText());
 
 
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -88,7 +108,22 @@ public class sberSiteTest {
 
     public void fillField(By by ,String val)
     {
+        //клик для того, чтобы окно с выбором даты исчезло
+        driver.findElement(by).click();
+
         driver.findElement(by).clear();
         driver.findElement(by).sendKeys(val);
+    }
+
+    public void fillFieldDate(By by, Date date)
+    {
+        SimpleDateFormat simpliDateForm = new SimpleDateFormat("dd.MM.YYYY");
+        String dateString = simpliDateForm.format(date);
+        //клик для того, чтобы окно с выбором даты исчезло
+        driver.findElement(by).click();
+
+        driver.findElement(by).clear();
+        driver.findElement(by).sendKeys(dateString);
+
     }
 }
