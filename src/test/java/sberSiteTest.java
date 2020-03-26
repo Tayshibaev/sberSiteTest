e976b6f17e95356f49f2a3b5527671205b8beef1
Timur <tayshibaevtb@gmail.com>
2020-03-26 18:37:44 +0300

    2

--- src/test/java/sberSiteTest.java
+++ src/test/java/sberSiteTest.java
@@ -14,28 +14,25 @@ public class sberSiteTest {
     WebDriver driver;
     String url = "https://www.sberbank.ru/ru/person";
 
-
     @Before
-    public void setUp()
-      {
-        System.setProperty("webdriver.chrome.driver","drv/chromedriver.exe");
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "drv/geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);//задаем тайм аут по умолчанию ожидания поиска
         driver.manage().window().maximize();
-      }
+    }
 
-      @Test
-      public void test()
-      {
-          driver.get(url);
-          driver.findElement(By.xpath("//a[text()='Страхование']")).click();
-          Wait<WebDriver> wait = new WebDriverWait(driver,5,15000);
-          wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(""))));
-      }
+    @Test
+    public void test() {
+        driver.get(url);
+        driver.findElement(By.xpath("//a[text()='Страхование']")).click();
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 15000);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(""))));
+    }
 
-      @After
-      public void tearDown()
-      {
-          driver.quit();
-      }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
