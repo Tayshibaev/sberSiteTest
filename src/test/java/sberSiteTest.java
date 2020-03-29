97ccd77ef0204d89e97bea0167b6de5425b17ecb
Timur <tayshibaevtb@gmail.com>
2020-03-29 13:46:02 +0300

    Готовый тест версия 1

--- src/test/java/sberSiteTest.java
+++ src/test/java/sberSiteTest.java
@@ -67,6 +67,12 @@ public class sberSiteTest {
         fillField(By.id("person_middleName"),"Магаданович");
         driver.findElement(By.xpath("//label[text()='Женский']")).click();
 
+        //заполнение полей паспорта
+        fillField(By.id("passportSeries"),"4456");
+        fillField(By.id("passportNumber"),"445656");
+        fillFieldDate(By.id("documentDate"),dateBirthPerson.getTime());
+
+
 
         Assert.assertEquals("Ivanushka",
                 driver.findElement(By.id("surname_vzr_ins_0")).getAttribute("value"));
@@ -80,10 +86,17 @@ public class sberSiteTest {
         Assert.assertEquals("Магаданович",
                 driver.findElement(By.id("person_middleName")).getAttribute("value"));
 
+        Assert.assertEquals("4456",
+                driver.findElement(By.id("passportSeries")).getAttribute("value"));
+        Assert.assertEquals("445656",
+                driver.findElement(By.id("passportNumber")).getAttribute("value"));
+
         Assert.assertEquals(smp.format(dateBirthPeople.getTime()),
                 driver.findElement(By.id("birthDate_vzr_ins_0")).getAttribute("value"));
         Assert.assertEquals(smp.format(dateBirthPerson.getTime()),
                 driver.findElement(By.id("person_birthDate")).getAttribute("value"));
+        Assert.assertEquals(smp.format(dateBirthPerson.getTime()),
+                driver.findElement(By.id("documentDate")).getAttribute("value"));
 
         driver.findElement(By.xpath("//button[contains(text(),'Продолжить')]")).click();
 
